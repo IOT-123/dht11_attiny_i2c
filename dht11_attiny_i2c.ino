94409bf6abc9e703ba8a188b26241a9043986967
IOT123 <33011946+IOT-123@users.noreply.github.com>
2018-05-22 09:33:03 +1000

    Remove SoftwareSerial, add METAS
    Use of PROGMEM (99%  memory without)

--- dht11_attiny_i2c.ino
+++ dht11_attiny_i2c.ino
@@ -11,118 +11,189 @@
  * SDA PB0
  * DHT11 PB1
  * SCL PB2
- * RX  PB3
- * TX PB4 
  */
 
 #include <Wire.h> //SDA pin5/PB0, SCL pin7/PB2
 #include "DHT.h"
 
-#define USE_SOFTWARE_SERIAL 0
-#define LIMIT_PROPERTIES 0  // if memory problems, set this to 1 and just get temp/humidity
-#define PIN_DHT 1
-#define ADDRESS_SLAVE 8
+#define arraySize(x)       (sizeof(x) / sizeof(x[0]))
+#define NVC_NUM_STAGES 3
+#define ADDRESS_SLAVE 10
+#define PIN_SENSOR 1
 
-#if (USE_SOFTWARE_SERIAL)
-  #include <SoftwareSerial.h>
-  #define PIN_RX    3   
-  #define PIN_TX    4   
-  SoftwareSerial mySerial(PIN_RX, PIN_TX);
+#define TIME_RESPONSE_MS 0 
+#if (TIME_RESPONSE_MS)
+  unsigned long startMillis; 
 #endif
+
+struct nvc
+{
+    char Name[16];
+    char Value[16];
+    bool Continue;
+};
+
+#define META_COUNT 8
+const static char m1[] PROGMEM = "ASSIM_NAME"; 
+const static char m2[] PROGMEM = "DHT11"; 
+const static char m3[] PROGMEM = "1"; 
+const static char m4[] PROGMEM = "ASSIM_VERSION"; 
+const static char m5[] PROGMEM = "1"; 
+const static char m6[] PROGMEM = "1"; 
+const static char m7[] PROGMEM = "ASSIM_ROLE"; 
+const static char m8[] PROGMEM = "SENSOR"; 
+const static char m9[] PROGMEM = "1"; 
+const static char m10[] PROGMEM = "POWER_DOWN"; 
+const static char m11[] PROGMEM = "1"; 
+const static char m12[] PROGMEM = "1"; 
+const static char m13[] PROGMEM = "PREPARE_MS"; 
+const static char m14[] PROGMEM = "140000"; 
+const static char m15[] PROGMEM = "1"; 
+const static char m16[] PROGMEM = "RESPONSE_MS"; 
+const static char m17[] PROGMEM = "50"; 
+const static char m18[] PROGMEM = "1"; 
+const static char m19[] PROGMEM = "MQTT_TOPIC"; 
+const static char m20[] PROGMEM = "TEMP_HUMID"; 
+const static char m21[] PROGMEM = "1"; 
+const static char m22[] PROGMEM = "VCC_MV"; 
+const static char m23[] PROGMEM = ""; 
+const static char m24[] PROGMEM = "0"; 
+
+const char* const _metas[] PROGMEM = { m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15, m16, m17, m18, m19, m20, m21, m22, m23, m24 };
+
+nvc _props[5] ={
+    {"Humidity (%)", "", true},
+    {"Temperature (C)", "", true},
+    {"Temperature (F)", "", true},
+    {"Temperature (K)", "", true},
+    {"Dew Point (C)", "", false}
+};
+
+volatile int _packetStage = 0;
+volatile int _propertyIndex = 0;
+volatile bool _metasConfirmed = false;
+volatile int _metaIndex = 0;
+uint16_t _vcc; 
 
 dht _dht;
-int _packetStage = 0;
-int _propertyIndex = 0;
-
-#define MAX_ENTRY_SIZE 16
-#define NUM_COLUMNS 3
-
-#if (!LIMIT_PROPERTIES) 
-#define NUM_ROWS 5
-char _properties[][NUM_COLUMNS][MAX_ENTRY_SIZE] = {
-    {"Humidity (%)", "", "1"},
-    {"Temperature (C)", "", "1"},
-    {"Temperature (F)", "", "1"},
-    {"Temperature (K)", "", "1"},
-    {"Dew Point (C)", "", "0"},
-};
-#else
-#define NUM_ROWS 2
-char _properties[][NUM_COLUMNS][MAX_ENTRY_SIZE] = {
-    {"Humidity (%)", "", "1"},
-    {"Temperature (C)", "", "0"}
-};
-#endif
 
 void setup()
 {
-  Wire.begin(ADDRESS_SLAVE);                
+  _vcc = getVcc();
+  Wire.begin(8);  
+  Wire.onReceive(receiveEvent);                  
   Wire.onRequest(requestEvent);
-#if (USE_SOFTWARE_SERIAL)
-  mySerial.begin(9600);
-  mySerial.println("DHT11 TEST PROGRAM ");
-  mySerial.print("LIBRARY VERSION: ");
-  mySerial.println(DHT11LIB_VERSION);
-  mySerial.println();
-#endif
 }
 
-void loop()
+void loop(){}
+
+void receiveEvent (int howMany) 
 {
-  delay(100);
+  byte buf[10];
+  int i;
+  for (i=0; i<howMany; i++)
+  {
+    buf[i] = Wire.read();     // receive byte as a character
+  }
+  if((buf[0] == 1) && (howMany == 1)){
+    _metasConfirmed = true;
+    _packetStage = 0;
+    _propertyIndex = 0;
+  }
 }
 
 void requestEvent() {
-  getProperties();
-  Wire.write(_properties[_propertyIndex][_packetStage]);
-  _packetStage++;
-  if (_packetStage == NUM_COLUMNS){
+  char currentPacket[16];
+  int propCount = 0;
+  if (_metasConfirmed){ 
+    if (_propertyIndex == 0){
+// get the sensor properties      
+      getProperties();
+    }
+    strcpy(currentPacket, nvcAsCharArray(_props[_propertyIndex], _packetStage));
+    propCount = arraySize(_props);
+// METADATA    
+  }else{ 
+    propCount = META_COUNT;
+    if (_metaIndex == 22){// if last metadata (VCC), only runtime entry
+      itoa(_vcc, currentPacket, 10);
+    }else{ // just a normal metadata item
+      //itoa(_metaIndex, currentPacket, 10);
+      strcpy_P(currentPacket, (char*)pgm_read_word(&(_metas[_metaIndex]))); 
+    }
+    _metaIndex++;
+  }
+  Wire.write(currentPacket); // send metadate or sensor property
+  _packetStage = _packetStage + 1;    
+  // go to next property if at last stage of current property
+  if (_packetStage == NVC_NUM_STAGES){
     _packetStage = 0;
     _propertyIndex++;
   }
-  if (_propertyIndex == NUM_ROWS){
+  // all properties processed?
+  if (_propertyIndex == propCount){
     _propertyIndex = 0;
-    // "0" in "Dew Point (°C)" should terminate requests to this slave
+    // "0" should terminate requests to this slave
   }
 }
+
 
 void getProperties(){
-  int chk =  _dht.read11(PIN_DHT);
-#if (USE_SOFTWARE_SERIAL)
-  mySerial.println("\n");
-  mySerial.println("Read sensor: ");
-  switch (chk)
-  {
-    case DHTLIB_OK: 
-    mySerial.println("OK"); 
-    break;
-    case DHTLIB_ERROR_CHECKSUM: 
-    mySerial.println("Checksum error"); 
-    break;
-    case DHTLIB_ERROR_TIMEOUT: 
-    mySerial.println("Time out error"); 
-    break;
-    default: 
-    mySerial.println("Unknown error"); 
-    break;
-  }
-#endif  
-  dtostrf(_dht.humidity,2,2,_properties[0][1]);
-  dtostrf(_dht.temperature,2,2,_properties[1][1]);
-#if (!LIMIT_PROPERTIES)  
-  dtostrf(Fahrenheit(_dht.temperature),2,2,_properties[2][1]);
-  dtostrf(Kelvin(_dht.temperature),2,2,_properties[3][1]);
-  dtostrf(dewPoint(_dht.temperature, _dht.humidity),2,2,_properties[4][1]);
-#endif 
-#if (USE_SOFTWARE_SERIAL)
-  for (int i = 0; i < NUM_ROWS; i++){
-    mySerial.print( _properties[i][0]);
-    mySerial.print(": ");
-    mySerial.println(_properties[i][1]);
-  }
-#endif
+  int chk =  _dht.read11(PIN_SENSOR);
+  dtostrf(_dht.humidity,2,2,_props[0].Value);
+  dtostrf(_dht.temperature,2,2,_props[1].Value);
+  dtostrf(Fahrenheit(_dht.temperature),2,2,_props[2].Value);
+  dtostrf(Kelvin(_dht.temperature),2,2,_props[3].Value);
+  dtostrf(dewPoint(_dht.temperature, _dht.humidity),2,2,_props[4].Value);
 }
 
+char* nvcAsCharArray(nvc nvc, int packetStage){
+  switch (packetStage){
+    case 0:
+      return nvc.Name;
+      break;
+    case 1: 
+        #if (TIME_RESPONSE_MS)
+          unsigned long currentMillis;
+          currentMillis = millis();
+          char millis[16];
+          itoa(currentMillis - startMillis, millis, 10);
+          return millis;
+        #endif
+      return nvc.Value;
+      break;
+    case 2:
+      return nvc.Continue ? "1" : "0";
+      break;
+    default:
+      char result[16];
+      itoa(packetStage, result, 10);
+      return result;
+  }
+}
+
+// https://www.avrfreaks.net/forum/attiny85-vcc-measurement-skews-higher-vcc-voltages
+//5v = 6393, 6504
+//3.3V 3430
+uint16_t getVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+  delay(2); // Wait for Vref to settle
+  uint16_t result = 0;
+  for (int x = 0; x < 32; x++){
+    ADCSRA |= _BV(ADSC); // Start conversion
+    while (bit_is_set(ADCSRA,ADSC)); // measuring
+    if (x > 15){
+      result += (int16_t)((int16_t)(ADC - result) / 2);
+    }
+    else{
+      result = ADC;
+    }
+  }
+  uint16_t voltage = 1125300 / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return voltage;
+}
 
 //Celsius to Fahrenheit conversion
 double Fahrenheit(double celsius)
